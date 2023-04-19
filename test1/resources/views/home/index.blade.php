90eb895bdb81c6d0db2b0d4aa64934025023b184
AlumnosInf <AlumnosInf@soportetiu.local>
2023-04-19 09:49:18 -0400

    commit

---
+++ test1/resources/views/home/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portada</title>
+</head>
+<body>
+    <h2>Portada de sitio</h2>
+    <a href="/login">Inicio sesión</a>
+</body>
+</html>
